a668b1636f08789c3ba648f8338d3506f6c29bf3
ron <rpicard92@gmail.com>
2018-12-04 10:54:58 -0600

    flake8 conformance on resert and setinitialstate

--- src/plugins/Reset/Reset/__init__.py
+++ src/plugins/Reset/Reset/__init__.py
@@ -12,34 +12,41 @@ logger = logging.getLogger('Reset')
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)  # By default it logs to stderr..
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(log_format)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
+# resets the token values of the places to the values
+# that they were when SetInitialState plugin ran
 class Reset(PluginBase):
     def main(self):
         core = self.core
         root_node = self.root_node
         active_node = self.active_node
+        branch = 'master'
+        message = 'Python plugin updated the model'
 
-        def per_node(node): 
-            meta_type=core.get_attribute(core.get_meta_type(node),'name')
-            name = core.get_attribute(node,'name')
+        # called on each node
+        def per_node(node):
+            meta_type = core.get_attribute(core.get_meta_type(node), 'name')
+            name = core.get_attribute(node, 'name')
             if(meta_type == 'Place'):
                 if name in places_dict:
-                    core.set_attribute(node,'Tokens',str(places_dict[name]))
+                    core.set_attribute(node, 'Tokens', str(places_dict[name]))
                 else:
-                    logger.info('[INFO]: Place must have been added after SetIniitalState plugin has run')
+                    message = 'Place added after SetIniitalState plugin run'
+                    logger.info('[INFO]: ' + message)
 
+        # gets the initial state attribute on the network
         def get_init_state(node):
-            name=core.get_attribute(node,'name')
+            name = core.get_attribute(node, 'name')
             if(name == 'Network'):
-                return json.loads(core.get_attribute(node,'InitialState'))
+                return json.loads(core.get_attribute(node, 'InitialState'))
             else:
                 logger.info('[INFO]: Not the Network node')
 
         places_dict = get_init_state(active_node)
         self.util.traverse(active_node, per_node)
-        commit_info = self.util.save(root_node, self.commit_hash, 'master', 'Python plugin updated the model')
-    
+        self.util.save(root_node, self.commit_hash, branch, message)
--- src/plugins/SetInitialState/SetInitialState/__init__.py
+++ src/plugins/SetInitialState/SetInitialState/__init__.py
@@ -12,35 +12,44 @@ logger = logging.getLogger('SetInitialState')
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)  # By default it logs to stderr..
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(log_format)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
+# sets the saves the token values of the places in currrent
+# state so that when the rest plug runs, it will return the
+# composition to these values
 class SetInitialState(PluginBase):
     def main(self):
         core = self.core
         root_node = self.root_node
         active_node = self.active_node
-        
-        def per_node(node): 
-            meta_type=core.get_attribute(core.get_meta_type(node),'name')
-            name = core.get_attribute(node,'name')
-            if(meta_type == 'Place'):
-                places_dict[name] = core.get_attribute(node,'Tokens')
+        places_dict = {}
+        branch = 'master'
+        message = 'Python plugin updated the model'
 
-        def set_init_state(node,places):
-            name = core.get_attribute(node,'name')
+        # called on each node
+        def per_node(node):
+            meta_type = core.get_attribute(core.get_meta_type(node), 'name')
+            name = core.get_attribute(node, 'name')
+            if(meta_type == 'Place'):
+                places_dict[name] = core.get_attribute(node, 'Tokens')
+
+        # sets the initial state attribute on the network
+        def set_init_state(node, places_dict):
+            name = core.get_attribute(node, 'name')
             if(name == 'Network'):
-                core.set_attribute(node,'InitialState',places)
+                core.set_attribute(node, 'InitialState', places_dict)
             else:
                 logger.info('[INFO]: Not Network node')
 
-
+        # make a json string from a dictionary
         def make_json_string(dictionary):
-            return (str(json.dumps(dictionary, indent=4, separators=(',', ': '))))
+            json_obj = json.dumps(dictionary, indent=4, separators=(',', ': '))
+            return (str(json_obj))
 
-        places_dict = {}
         self.util.traverse(active_node, per_node)
-        set_init_state(active_node,make_json_string(places_dict))
-        commit_info = self.util.save(root_node, self.commit_hash, 'master', 'Python plugin updated the model')
+        set_init_state(active_node, make_json_string(places_dict))
+        self.util.save(root_node, self.commit_hash, branch, message)
